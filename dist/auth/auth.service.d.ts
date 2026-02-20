251c71f8976cfa91b6d4eee771274888c2b3b151
rabertcaamano <raristides.caamano@gmail.com>
2026-02-19 22:29:39 -0300

    Eliminada la carpeta dist

--- dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtService } from '@nestjs/jwt';
-export declare class AuthService {
-    private readonly jwtService;
-    constructor(jwtService: JwtService);
-    login(username: string, pass: string): Promise<{
-        access_token: string;
-    }>;
-}
